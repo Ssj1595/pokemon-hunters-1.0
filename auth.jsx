3d89f81efbb77d4d6f5e8cea0e79deb7c016c4ac
Shantan SJ <ssjshantan@gmail.com>
2026-05-12 16:40:03 +0530

    Updated UI code

--- auth.jsx
+++ auth.jsx
@@ -15,6 +15,26 @@ const ALLOWED_EMAILS = Object.freeze([
 
 window.ALLOWED_EMAILS = ALLOWED_EMAILS;
 
+async function sha256(message) {
+
+  const msgBuffer = new TextEncoder().encode(message);
+
+  const hashBuffer = await crypto.subtle.digest(
+    "SHA-256",
+    msgBuffer
+  );
+
+  const hashArray = Array.from(
+    new Uint8Array(hashBuffer)
+  );
+
+  const hashHex = hashArray
+    .map(b => b.toString(16).padStart(2, "0"))
+    .join("");
+
+  return hashHex;
+}
+
 async function getAdminCreds() {
   try {
     const res = await fetch(
